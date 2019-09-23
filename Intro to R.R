072ffe6091c1b02be913834d9431e22c26ad1a39
Jeff <jeffprosenthal@gmail.com>
2019-09-23 14:30:56 -0400

    Changed Intro to R script
    Opened Rproj and added my name to Intro to R.R script

--- Intro to R.R
+++ Intro to R.R
@@ -1,3 +1,4 @@
+<<<<<<< 1dd05e087b4aa2cc0707b82bb5d1d4c4f6a180d3
 # R script to accopany Intro to R
 # Copy for RStudent 2019
 ######################################################
@@ -7,3 +8,14 @@
 # 
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
+=======
+# R script to accompany Intro to R
+# Copy for Jeff Rosenthal as RStudent 2019
+######################################################
+
+# You will see that you can put non-executable "comment" lines
+# in your script by putting "#" at the beginning of the lines.
+# 
+# In fact, you can put comments anywhere on a line as long as 
+# you put "#" before them.
+>>>>>>> Added my name
